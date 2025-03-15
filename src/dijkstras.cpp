bcfb8c99516997143c37b78d754b975ee9ef4296
Emily Tang <tange8@uci.edu>
2025-03-15 06:33:33 +0000

    print

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -57,6 +57,7 @@ void print_path(const vector<int>& path, int total) {
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
          if (i < path.size() - 1) cout << " ";
+        cout << endl;
     }
-    cout << " \nTotal cost is " << total << endl;
+    cout << "\nTotal cost is " << total << endl;
 }
